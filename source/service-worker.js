4b44ffd5fd28abe127b20663a224ced2ff7c13d5
peterfisher <c3T-y7J-vo3-qpg>
2019-04-08 08:55:37 +0100

    Removing prefix slash

--- source/service-worker.js
+++ source/service-worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'howtocodewell-' + CACHE_VERSION;
 const URLS_TO_CACHE = [
   '/',
   '/assets/css/site.css',
-  '/schedule',
-   '/sponsor',
+  'schedule',
+   'sponsor',
    OFFLINE_URL
 ];
 
